60b4f75d49d96633c0c6135680bef42e5292f4a9
RupeshKeesaram <keesaramrupesh@gmail.com>
2022-08-14 19:22:26 +0530

    Validations using Beans

--- src/main/java/com/udemy/school/controllers/ContactController.java
+++ src/main/java/com/udemy/school/controllers/ContactController.java
@@ -2,12 +2,19 @@ package com.udemy.school.controllers;
 
 import com.udemy.school.models.Contact;
 import com.udemy.school.services.ContactService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -21,17 +28,23 @@ public class ContactController {
 
 
     @RequestMapping("/contact")
-    public String displayCourses()
+    public String displayCourses(Model model)
     {
+        model.addAttribute("contact",new Contact());
         return "contact.html";
     }
 
 
 
     @RequestMapping(value = "/saveMsg",method = RequestMethod.POST)
-    public ModelAndView saveMsg(Contact contact)
+    public String saveMsg(@Valid @ModelAttribute("contact") Contact contact, Errors errors)
     {
+        if(errors.hasErrors())
+        {
+            log.warn(errors.toString());
+            return "contact.html";
+        }
         contactService.saveMsgDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- src/main/java/com/udemy/school/models/Contact.java
+++ src/main/java/com/udemy/school/models/Contact.java
@@ -3,12 +3,31 @@ package com.udemy.school.models;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class Contact {
-    String name;
-    String mobileNum;
-    String email;
-    String subject;
-    String message;
 
+    @NotBlank(message = "name must be greater than zero")
+    @Size(min = 3,message = "name must be at least 3 characters long")
+    String name;
+
+    @NotBlank(message = "name must be greater than zero")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "mobile numbers must be 10 digits")
+    String mobileNum;
+
+    @NotBlank(message = "name must be greater than zero")
+    @Email(message = "Please provide a valid emailId")
+    String email;
+
+    @NotBlank(message = "name must be greater than zero")
+    @Size(min=5,message = "subject should be at least 5 characters")
+    String subject;
+
+    @NotBlank(message = "name must be greater than zero")
+    @Size(min=10,message = "Message should be at least 10 characters long")
+    String message;
 }
